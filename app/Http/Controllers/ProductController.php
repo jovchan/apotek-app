989f8bf13cfd26cc6471c3d571adc6d84b64fb17
jovcheng <jovianchandras@gmail.com>
2024-07-15 11:22:37 +0700

    berhasil narik data

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,7 +12,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return view('admin.products.index');
+        $products = Product::all();
+        return view('admin.products.index', ['products' => $products]);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ Route::get('/admin', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/admin/products', 'ProductController@index');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
